2f00a340ae7a9cfa218db5b3a90cc2012a50a4dd
Shane Sniteman <ssniteman@gmail.com>
2014-02-03 17:35:44 -0500

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+$( ".register" ).click(function() {
+  $(this).addClass('active');
+});
+
+
+
+$( ".register" ).click(function() {
+  $(".form-horizontal").toggle();
+});
+
+$( ".submit" ).click(function() {
+	$(".form-horizontal").toggle();
+});
+
